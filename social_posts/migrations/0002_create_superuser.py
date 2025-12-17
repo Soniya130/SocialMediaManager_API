3f09ccfdd31c414cf4b587e185bbffde8b4af3ac
Soniya130 <soniyaapatil30@gmail.com>
2025-12-17 14:59:37 +0530

    Add superuser migration

---
+++ social_posts/migrations/0002_create_superuser.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+def create_superuser(apps, schema_editor):
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='soniyapatil130',
+            email='soniyaapatil30@gmail.com',
+            password='Soniya@130'
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('social_posts', '0001_initial'),  # Replace with the last migration of your app
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
